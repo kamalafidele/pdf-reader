3a47c655c247008bf4715661af40adfbc7a83b4e
kamalafidele <kamalafizzet45@gmail.com>
2023-08-27 17:00:30 +0200

    changed the implementation logic a bit

--- src/routes/post.extract-pdf.js
+++ src/routes/post.extract-pdf.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { PythonShell } = require('python-shell');
 const multer = require('multer');
 const fs = require('fs');
+const pdf = require('pdf-parse');
 
 const storage = multer.diskStorage({
     destination: './uploads/',
@@ -26,10 +27,13 @@ router.post(
             return res.status(400).json({ error: 'The file must be pdf' });
         }
 
+        let dataBuffer = fs.readFileSync('./uploads/saved_file.pdf');
+        const { metadata, text: extractedText } = await pdf(dataBuffer);
+
         const text = await PythonShell.run('app.py', null);
         fs.unlinkSync(file.path);
 
-        return res.status(200).json({ text });
+        return res.status(200).json({ text: `${extractedText}` });
     } catch (e) {
         return res.status(500).json(e);
     }
